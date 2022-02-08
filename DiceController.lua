cc5554c7d35feb5c6ec1089c3bc96ee17df6682c
zerasul <zerasul@gmail.com>
2022-02-08 22:54:21 +0100

    fixing sound and updating interface version

--- DiceController.lua
+++ DiceController.lua
@@ -13,9 +13,9 @@ SlashCmdList["ELLIDICE"] = function(msg, editbox)
     local number=math.random(1,20)
     printChatMessage(number)
     if number <= 5 then
-        PlaySoundFile("Sound\\Creature\\HoodWolf\\HoodWolfTransformPlayer01.ogg","master")
+        PlaySound(9278)
         printChatMessage("MUAHAHAHAHA. THE CURSE")
     elseif number>=15 then
-        PlaySoundFile("Sound\\Character\\Gnome\\GnomeFemaleChooChoo01.ogg", "master")
+        PlaySound(7640)
     end
 end
